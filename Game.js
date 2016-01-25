aed0ac268b5d2c661f645760dac5ed39cccf47ea
rlaqlc <rlaqlc@hotmail.com>
2016-01-24 19:29:37 -0700

    testing

--- Game.js
+++ Game.js
@@ -1,3 +1,53 @@
-hi kim bit
-hi jay
-let's start this
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+var manager = new THREE.LoadingManager();
+
+
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+// instantiate a loader
+var loader = new THREE.ImageLoader();
+
+// load a image resource
+loader.load(
+    // resource URL
+    'textures/skyboxsun25degtest.png',
+    // Function when resource is loaded
+    function ( image ) {
+        // do something with it
+
+        // like drawing a part of it on a canvas
+        var canvas = document.createElement( 'canvas' );
+        var context = canvas.getContext( '2d' );
+        context.drawImage( image, 100, 100 );
+    },
+    // Function called when download progresses
+    function ( xhr ) {
+        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+    },
+    // Function called when download errors
+    function ( xhr ) {
+        console.log( 'An error happened' );
+    }
+);
+
+var render = function () {
+    requestAnimationFrame( render );
+
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+
+    renderer.render(scene, camera);
+};
+
+render();
